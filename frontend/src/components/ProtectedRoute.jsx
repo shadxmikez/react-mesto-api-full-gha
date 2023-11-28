cf8bb25038fc638f20dd175059b3ceaff55704c5
Dave Ivashevic <shadxmikez@yndex.ru>
2023-11-28 14:45:09 +0300

    002

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRouteElement = ({ element: Component, ...props }) => {
 	return (
-		props.userLoggedIn ? Component : <Navigate to="/signin" replace />
+		props.loggedIn ? Component : <Navigate to="/signin" replace />
 	)
 }
 
